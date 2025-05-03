d837ec89bf387f0211cd6c3928bfa4decde6848c
ClarenceLaria <clarencelaria@gmail.com>
2025-05-03 19:20:43 +0300

    Bottom NavBar updates

--- lib/screens/entry_point.dart
+++ lib/screens/entry_point.dart
@@ -16,7 +16,7 @@ class _EntryPointState extends State<EntryPoint>{
 
   final List<Widget> screens = [
     const HomeScreen(),
-    const Center(child: PregnancyHomeScreen()),
+    const PregnancyHomeScreen(),
     const Center(child: Text('Notification')),
     const Center(child: Text('Friends')),
   ];
@@ -40,42 +40,58 @@ class _EntryPointState extends State<EntryPoint>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: Container(
-        color: Colors.transparent,
-        padding: const EdgeInsets.only(bottom: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ...List.generate(
-              buttonNavs.length, 
-              (index) => GestureDetector(
-                onTap: () => onTap(index),
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  margin: const EdgeInsets.symmetric(horizontal: 1),
-                  decoration: BoxDecoration(
-                    color: index == _selectedIndex ? Colors.black : Colors.white,
-                    borderRadius: BorderRadius.circular(40),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 30,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  padding: const EdgeInsets.all(20),
-                  child: Image.asset(
-                    buttonNavs[index], 
-                    color: index == _selectedIndex ? Colors.white : Colors.black,
-                  ),
+      extendBody: true,
+      body: Stack(
+        children:[
+          screens[_selectedIndex],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Colors.transparent,
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Container(
+                width: MediaQuery.of(context).size.width - 110,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.35),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              )
-            )
-          ],
-        ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ...List.generate(
+                      buttonNavs.length, 
+                      (index) => GestureDetector(
+                        onTap: () => onTap(index),
+                        child: Container(
+                          width: 60,
+                          height: 60,
+                          margin: const EdgeInsets.symmetric(horizontal: 1),
+                          decoration: BoxDecoration(
+                            color: index == _selectedIndex ? Colors.black : Colors.white,
+                            borderRadius: BorderRadius.circular(40),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 30,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          child: Image.asset(
+                            buttonNavs[index], 
+                            color: index == _selectedIndex ? Colors.white : Colors.black,
+                          ),
+                        ),
+                      )
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }  
