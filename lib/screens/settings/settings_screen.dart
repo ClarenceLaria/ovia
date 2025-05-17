3f063f1c70e4312649837bde6ec6b0b2659cf13e
ClarenceLaria <clarencelaria@gmail.com>
2025-05-17 19:51:20 +0300

    Settings screen updates

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ovia_app/api_connectors/login.dart';
 import 'package:ovia_app/features/settings/get_pregnant.dart';
 import 'package:ovia_app/features/settings/track_cycle.dart';
 import 'package:ovia_app/features/settings/track_pregnancy.dart';
+import 'package:ovia_app/screens/login_screen.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -11,7 +13,6 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-
   int _selectedIndex = 0;
   final List<Widget> tabs = [
     const TrackCycle(),
@@ -29,9 +30,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
       _selectedIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: const Color.fromARGB(255, 245, 245, 245),
       body: SafeArea(
         child: SingleChildScrollView(
@@ -43,12 +45,17 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 Row(
                   children: [
                     IconButton(
-                      onPressed: (){
+                      onPressed: () {
                         Navigator.pop(context);
-                      }, 
-                      icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black,),
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.black,
+                      ),
                     ),
-                    const SizedBox(width: 10,),
+                    const SizedBox(
+                      width: 10,
+                    ),
                     const Text(
                       'Settings',
                       style: TextStyle(
@@ -65,62 +72,115 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     fontWeight: FontWeight.w500,
                   ),
                 ),
-                const SizedBox(height: 16,),
+                const SizedBox(
+                  height: 16,
+                ),
                 Container(
                   decoration: const BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(16)),
                     color: Colors.white,
                   ),
                   padding: const EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  child: Column(
                     children: [
-                      const CircleAvatar(
-                        radius: 18,
-                        backgroundImage: AssetImage("assets/images/face.webp"),
-                      ),
-                      const SizedBox(width: 20,),
-                      const Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Emily Johnson',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              'emilyjohnson@gmail.com',
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w200,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                        width: 100,
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            // minimumSize: const Size(10, 30),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            // backgroundColor: Colors.white,
-                            // foregroundColor: Colors.black,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const CircleAvatar(
+                            radius: 18,
+                            backgroundImage:
+                                AssetImage("assets/images/face.webp"),
                           ),
-                          onPressed: () {}, 
-                          child: const Text('Edit Profile', style: TextStyle(fontSize: 10),),
-                        ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          const Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Emily Johnson',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'emilyjohnson@gmail.com',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w200,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                            width: 100,
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                // minimumSize: const Size(10, 30),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                // backgroundColor: Colors.white,
+                                // foregroundColor: Colors.black,
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                'Edit Profile',
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Sign out of your account',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                            width: 100,
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                              onPressed: () {
+                                Auth().signOut();
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const LoginScreen(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Log out',
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 16,),
+                const SizedBox(
+                  height: 16,
+                ),
                 const Text(
                   'My goal',
                   style: TextStyle(
@@ -128,8 +188,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     fontWeight: FontWeight.w500,
                   ),
                 ),
-                const SizedBox(height: 16,),
-                 Container(
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
                   width: double.infinity,
                   decoration: BoxDecoration(
                     color: Colors.white,
@@ -140,23 +202,30 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       ...List.generate(
-                        buttonTabs.length, 
-                        (index) => GestureDetector(
-                          onTap: () => onTap(index),
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 4),
-                            decoration: BoxDecoration(
-                              color: index == _selectedIndex ? Colors.black : Colors.white,
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-                            child: Text(
-                              buttonTabs[index], 
-                              style: TextStyle(color: index == _selectedIndex ? Colors.white : Colors.black, fontSize: 12),
-                            ),
-                          ),
-                        )
-                      )
+                          buttonTabs.length,
+                          (index) => GestureDetector(
+                                onTap: () => onTap(index),
+                                child: Container(
+                                  margin:
+                                      const EdgeInsets.symmetric(horizontal: 4),
+                                  decoration: BoxDecoration(
+                                    color: index == _selectedIndex
+                                        ? Colors.black
+                                        : Colors.white,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 12, vertical: 10),
+                                  child: Text(
+                                    buttonTabs[index],
+                                    style: TextStyle(
+                                        color: index == _selectedIndex
+                                            ? Colors.white
+                                            : Colors.black,
+                                        fontSize: 12),
+                                  ),
+                                ),
+                              ))
                     ],
                   ),
                 ),
