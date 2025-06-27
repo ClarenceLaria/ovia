d2a469ab2bb00299896081aa87288d35c8641fc9
ClarenceLaria <clarencelaria@gmail.com>
2025-06-27 16:55:45 +0300

    Splash router internet connectivity check updates

--- lib/features/home/splash_router.dart
+++ lib/features/home/splash_router.dart
@@ -3,6 +3,7 @@ import 'package:ovia_app/api_connectors/apis.dart';
 import 'package:ovia_app/screens/entry_point.dart';
 import 'package:ovia_app/screens/login_screen.dart';
 import 'package:ovia_app/screens/user_info_setup.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class SplashRouter extends StatelessWidget {
   const SplashRouter({super.key});
@@ -18,7 +19,43 @@ class SplashRouter extends StatelessWidget {
   //   }
   // }
 
-  Future<Widget> _determineStartPage() async {
+  Widget _buildNoInternetPage(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Icon(Icons.wifi_off, size: 100, color: Colors.grey),
+            const SizedBox(height: 16),
+            const Text(
+              'No Internet Connection',
+              style: TextStyle(fontSize: 24, color: Colors.grey),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                // Retry logic can be added here
+                final connectivityResult = await Connectivity().checkConnectivity();
+                if (connectivityResult != ConnectivityResult.none) {
+                  // If internet is available, navigate to the start page
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SplashRouter()),
+                  );
+                }
+              },
+              child: const Text('Retry'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Widget> _determineStartPage(BuildContext context) async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if(connectivityResult == ConnectivityResult.none){
+      return _buildNoInternetPage(context);
+    }
   try {
     final result = await APIs().fetchUserInfo();
 
@@ -38,7 +75,7 @@ class SplashRouter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<Widget>(
-      future: _determineStartPage(),
+      future: _determineStartPage(context),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Scaffold(
